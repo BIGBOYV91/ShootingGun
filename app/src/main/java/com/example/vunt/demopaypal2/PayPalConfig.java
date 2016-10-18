d7e91e09b49edf327dcd0cfd2711f143beea5b5b
tuanvu.fithou@gmail.com <tuanvu.fithou@gmail.com>
2016-10-18 22:23:02 +0700

    code paypal

---
+++ app/src/main/java/com/example/vunt/demopaypal2/PayPalConfig.java
@@ -0,0 +1,8 @@
+package com.example.vunt.demopaypal2;
+
+/**
+ * Created by VUNT on 14/10/2016.
+ */
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AapjZ9X_HIX0lhLBW7kzQn3_9KXUde8xAF2e2-Fpxd-kpyKkvR_Twf0I1u43IX2KQoVaGqLLM78pwJju";
+}
